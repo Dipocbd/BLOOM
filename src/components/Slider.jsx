3e1124aed53d80c6544e2194c26ab78c6d4bc945
Dipocbd <dipoojo6@gmail.com>
2022-10-12 16:54:23 -0700

    Added some Styled Components

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,10 +5,42 @@ import styled from 'styled-components'
 const Slider = () => {
   return (
     <Container>
-        <Arrow>
+        <Arrow direction="left">
             <ArrowLeftOutlined />
         </Arrow>
-        <Arrow>
+        <Wrapper>
+            <Slide >
+                <ImgContainer>
+            <Image src = "https://images.unsplash.com/photo-1664913161359-6d502b997f92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60" BORDER = "0" />
+            </ImgContainer>
+            <InfoContainer>
+                <Title>POPULAR SALE</Title>
+                <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+                <Button>SHOP NOW</Button>
+            </InfoContainer>
+            </Slide>
+            <Slide>
+                <ImgContainer>
+            <Image src = "https://images.unsplash.com/photo-1664913161359-6d502b997f92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60" BORDER = "0" />
+            </ImgContainer>
+            <InfoContainer>
+                <Title>SUMMER SALE</Title>
+                <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+                <Button>SHOP NOW</Button>
+            </InfoContainer>
+            </Slide>
+            <Slide>
+                <ImgContainer>
+            <Image src = "https://images.unsplash.com/photo-1664913161359-6d502b997f92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fFM0TUtMQXNCQjc0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60" BORDER = "0" />
+            </ImgContainer>
+            <InfoContainer>
+                <Title>WINTER SALE</Title>
+                <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+                <Button>SHOP NOW</Button>
+            </InfoContainer>
+            </Slide>
+        </Wrapper>
+        <Arrow direction= "right">
             <ArrowRightOutlined />
         </Arrow>
     </Container>
@@ -19,8 +51,8 @@ const Container = styled.div`
  width:100%;
  height:100vh;
  display:flex;
- background-color:coral;
- position:relative;`
+ position:relative;
+ overflow:hidden;`
 
  const Arrow = styled.div`
  width:50px;
@@ -33,6 +65,48 @@ const Container = styled.div`
  position:absolute;
  top:0;
  bottom:0;
- margin:auto;`
+ left: ${props=> props.direction === "left" && "10px"};
+ right:${props=> props.direction === "right" && "10px"};
+ margin:auto;
+ cursor:pointer;
+ opacity: 0.5;`
+
+ const Wrapper = styled.div`
+ height:100%;
+ display:flex;
+ `
+
+ const Slide = styled.div`
+ width:100vw;
+ height:100vh;
+ display:flex;
+ align-items:center;`;
+
+ const ImgContainer= styled.div`
+ height:100%;
+ flex:1`;
+
+ const Image = styled.img`
+ height:80%`;
+
+ const InfoContainer = styled.div`
+ flex:1;
+ padding:50px`;
+
+ const Title = styled.h1`
+ font-sixe:70px;`
+
+ const Desc = styled.p`
+ margin:50px 0px;
+ font-size:20px;
+ font-weight:500;
+ letter-spacing:3px;`
+ 
+ const Button = styled.button`
+ padding:10px;
+ font-six=ze:20px;
+ background-color:transparent;
+ cursor:pointer;`
+ 
 
 export default Slider
